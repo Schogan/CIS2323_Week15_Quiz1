990f7700fb285cfb21554d53f658effc594f8c72
Schogan <navy15@gmail.com>
2018-11-28 18:43:14 -0600

    sorta

--- CIS2323_Week15_Quiz1.java
+++ CIS2323_Week15_Quiz1.java
@@ -5,8 +5,15 @@ import java.io.IOException;
 
 public class CIS2323_Week15_Quiz1 {
 	public static void main(String[] args) {
-	Path filePath = Paths.get("C:\\Users\\schogan\\Desktop\\Projects\\CIS2323_Week15_Quiz1");
+		Path filePath = Paths.get("C:\\Users\\schogan\\Desktop\\Projects\\CIS2323_Week15_Quiz1");
 		
+		
+		int count = filePath.getNameCount();
+		
+		System.out.println("File name is " + filePath.getFileName());
+		for(int x = 0; x < count; ++x){
+			System.out.println("The containing folder is: " + filePath.getName(x));
+		}
 		try{
 			BasicFileAttributes attr = Files.readAttributes(filePath, BasicFileAttributes.class);
 			System.out.println("Creation time " + attr.creationTime()); 
